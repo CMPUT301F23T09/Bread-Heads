ea9e63ac9fa549a9f889e5c227e96f6f4fce6a24
Evan <ejdoerks@ualberta.ca>
2023-10-28 17:14:19 -0600

    created a menu when filter is tapped
    small menu when filter button is tapped, need to add stuff said button is tapped. Also changed the onOptionsItemSelected to use "If/Else" as android compalins with ID switching. idk why.

--- app/app/src/main/java/com/example/breadheadsinventorymanager/MainActivity.java
+++ app/app/src/main/java/com/example/breadheadsinventorymanager/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.breadheadsinventorymanager;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +9,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -28,11 +32,12 @@ import java.util.ArrayList;
  */
 public class MainActivity extends AppCompatActivity {
 
-    // INITIALIZE LIST DELETE BEFORE MERGING
+    // INITIALIZE LIST OBJECTS DELETE BEFORE MERGING
     private ItemList itemList;
     private ArrayAdapter<Item> itemArrayAdapter;
     private ListView itemListView;
 
+    private MenuItem filterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +46,9 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.user_icon);
 
-        // adapter stuff
+
+        // adapter stuff DELETE BEFORE MERGING!
+
         itemListView = findViewById(R.id.items_main_list);
 
         itemList = new ItemList();
@@ -49,31 +56,53 @@ public class MainActivity extends AppCompatActivity {
         itemListView.setAdapter(itemArrayAdapter);
 
         // test cases for sample data
-        Item item1 = new Item("22/01/2000", "this is a test case", "make", "model", "123456789", 12);
-        Item item2 = new Item("22/01/2000", "this is a test case", "make", "model", "123456789", 12);
-        Item item3 = new Item("22/01/2000", "this is a test case", "make", "model", "123456789", 12);
-        Item item4 = new Item("22/01/2000", "this is a test case", "make", "model", "123456789", 12);
+        Item item1 = new Item("22/01/2000", "this is test case 1", "make", "model", "123456789", 12);
+        Item item2 = new Item("22/01/2000", "this is test case 2", "make", "model", "123456789", 12);
+        Item item3 = new Item("22/01/2000", "this is test case 3", "make", "model", "123456789", 12);
+        Item item4 = new Item("22/01/2000", "this is test case 4", "make", "model", "123456789", 12);
         itemList.add(item1);
         itemList.add(item2);
         itemList.add(item3);
         itemList.add(item4);
         itemArrayAdapter.notifyDataSetChanged();
+
+        // END OF adapter stuff DELETE BEFORE MERGING!
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.topbar, menu);
+
+        // get the topbar button id
+        filterButton = menu.findItem(R.id.filter_menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                //Do stuff
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        int id = item.getItemId();
+        if(id == android.R.id.home ) {
+            // do stuff
+            return true;
+        } else if (id == R.id.filter_menu) {
+            // create menu for filtering items
+            filterButtonCLick();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
         }
+    }
+
+    /** handles the menu creation after the "filter button is tapped
+     *
+     */
+    private void filterButtonCLick() {
+            // shows the menu
+            PopupMenu popup = new PopupMenu(this, this.findViewById(R.id.filter_menu));
+            popup.getMenuInflater().inflate(R.menu.filter_menu, popup.getMenu());
+            popup.show();
+            // TODO make stuff happen when buttons are tapped
     }
 }
