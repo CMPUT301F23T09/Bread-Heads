c1c71cb937179746b5d7f4c46d320c2cddd0b48b
Zamam-Moeez <zmoeez@outlook.com>
2023-10-31 18:23:49 -0600

    updated tests

--- app/app/src/test/java/com/example/breadheadsinventorymanager/ItemListTest.java
+++ app/app/src/test/java/com/example/breadheadsinventorymanager/ItemListTest.java
@@ -1,7 +1,11 @@
 package com.example.breadheadsinventorymanager;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
+import junit.framework.TestCase;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,6 +13,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class ItemListTest {
+
 
     private Item item1;
     private Item item2;
@@ -35,21 +40,55 @@ public class ItemListTest {
 
     }
 
+    @Test
+    public void overriddenMethodsTest() {
+        ItemList list1 = new ItemList();
+        Item item1 = new Item("Jan 23", "Item 1", "Maker",
+                "My Model", "151SERIAL", 1050);
+        list1.add(item1);
+        ItemList list2 = new ItemList(list1);
+
+        assertEquals(list1, list2);
+
+        Item item2 = new Item("Jan 23", "Item 1", "Maker",
+                "My Model", "151SERIAL", 25);
+        list1.add(item2);
+
+        assertEquals(1050, list2.getSum(), 0.0);
+        assertEquals(1075, list1.getSum(), 0.0);
+
+        list1.remove(item1);
+
+        assertEquals(1, list1.size());
+        assertEquals(25, list1.getSum(), 0.0);
+
+        // can't remove index that doesn't exist
+        Assert.assertThrows(IndexOutOfBoundsException.class, () -> list1.remove(2));
+
+        list1.remove(0);
+        list2.remove(0);
+        assertEquals(0, list1.size());
+        assertEquals(list1, list2);
+
+        // can't remove index that doesn't exist
+        Assert.assertThrows(IndexOutOfBoundsException.class, () -> list1.remove(0));
+        assertFalse(list1.remove(item1));
+    }
 
     @Test
     public void testAddItem() {
 
         itemList.add(item1);
-        assertEquals(1, itemList.size());
+        TestCase.assertEquals(1, itemList.size());
 
         // Check the sum
-        assertEquals(10, itemList.getSum(), 0.001);
+        TestCase.assertEquals(10, itemList.getSum(), 0.001);
 
         itemList.add(item2);
-        assertEquals(2, itemList.size());
+        TestCase.assertEquals(2, itemList.size());
 
         // Check the sum
-        assertEquals(30, itemList.getSum(), 0.001);
+        TestCase.assertEquals(30, itemList.getSum(), 0.001);
     }
 
 
@@ -60,10 +99,10 @@ public class ItemListTest {
         itemList.add(item2);
 
         itemList.remove("1");
-        assertEquals(1, itemList.size());
-        assertEquals(20, itemList.getSum(), 0.001);
+        TestCase.assertEquals(1, itemList.size());
+        TestCase.assertEquals(20, itemList.getSum(), 0.001);
         itemList.remove("2");
-        assertEquals(0, itemList.getSum(), 0.001);
+        TestCase.assertEquals(0, itemList.getSum(), 0.001);
 
     }
 
@@ -76,9 +115,9 @@ public class ItemListTest {
 
         // Remove the item by index
         itemList.remove(item1);
-        assertEquals(20, itemList.getSum(), 0.001);
+        TestCase.assertEquals(20, itemList.getSum(), 0.001);
         itemList.remove(item2);
-        assertEquals(0, itemList.getSum(), 0.001);
+        TestCase.assertEquals(0, itemList.getSum(), 0.001);
 
     }
 
@@ -90,24 +129,22 @@ public class ItemListTest {
 
         // Remove the item by index
         itemList.remove(0);
-        assertEquals(20, itemList.getSum(), 0.001);
+        TestCase.assertEquals(20, itemList.getSum(), 0.001);
         itemList.remove(0);
-        assertEquals(0, itemList.getSum(), 0.001);
+        TestCase.assertEquals(0, itemList.getSum(), 0.001);
 
     }
 
     @Test
     public void testInitializeCollection() {
-        assertEquals(2, itemListC.size());
-        assertEquals(30, itemListC.getSum(), 0.001);
+        TestCase.assertEquals(2, itemListC.size());
+        TestCase.assertEquals(30, itemListC.getSum(), 0.001);
         // Remove the item by index
         itemListC.remove("2");
-        assertEquals(10, itemListC.getSum(), 0.001);
+        TestCase.assertEquals(10, itemListC.getSum(), 0.001);
         itemListC.remove("1");
-        assertEquals(0, itemListC.getSum(), 0.001);
+        TestCase.assertEquals(0, itemListC.getSum(), 0.001);
 
     }
-
-
 
 }
