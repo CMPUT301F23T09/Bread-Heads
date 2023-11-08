0c6ecb97e4a04f46791e4509d8140753daba80f9
Zamam-Moeez <zmoeez@outlook.com>
2023-11-07 18:15:15 -0700

    changes to tag, working on tests

--- app/app/src/main/java/com/example/breadheadsinventorymanager/TagList.java
+++ app/app/src/main/java/com/example/breadheadsinventorymanager/TagList.java
@@ -65,4 +65,45 @@ public class TagList extends ArrayList<Tag> {
     public boolean add(Tag tag) {
         return addTag(tag);
     }
+
+    /**
+     * Removes and returns the Tag at the specified index.
+     *
+     * @param index The index of the Tag to remove.
+     * @return The removed Tag.
+     */
+    public Tag remove(int index) {
+        if (index >= 0 && index < size()) {
+            return super.remove(index);
+        } else {
+            throw new IndexOutOfBoundsException("Index is out of bounds.");
+        }
+    }
+
+    /**
+     * Removes and returns the first Tag with the specified tag string.
+     *
+     * @param tag The tag string to remove.
+     * @return The removed Tag, or null if the tag is not found.
+     */
+    public Tag remove(String tag) {
+        for (Tag t : this) {
+            if (t.getTag().equals(tag)) {
+                super.remove(t);
+                return t;
+            }
+        }
+        return null; // Tag not found
+    }
+
+    /**
+     * Removes the first occurrence of the specified Tag.
+     *
+     * @param tag The Tag object to remove.
+     * @return true if the Tag was found and removed, false otherwise.
+     */
+    public boolean remove(Tag tag) {
+        return super.remove(tag);
+    }
+
 }
