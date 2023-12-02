7f81e6dc3f2a230dc222f43bad827743efae5869
Zamam-Moeez <zmoeez@outlook.com>
2023-12-02 12:06:30 -0700

    edit tag tests added

--- app/app/src/androidTest/java/com/example/breadheadsinventorymanager/ItemDetailsActivityTest.java
+++ app/app/src/androidTest/java/com/example/breadheadsinventorymanager/ItemDetailsActivityTest.java
@@ -4,16 +4,22 @@ import androidx.test.espresso.Espresso;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isNotChecked;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
@@ -26,35 +32,104 @@ public class ItemDetailsActivityTest {
     @Before
     //runs before each test it creates a sample item
     public void createSampleItem() {
-        //TODO: update tests, no longer add_item. Instead we click add_element followed by add_new_item
         onView(withId(R.id.add_element)).perform(click());
+        // Click on the "Add Tag" option
+        onView(withText("Add Tag")).perform(click());
+        // Type tag name in the EditText
+        onView(withId(R.id.tag_name_text)).perform(typeText("Test Tag"), closeSoftKeyboard());
+        onView(withId(android.R.id.button1)).perform(click());
+        onView(withId(R.id.add_element)).perform(click());
+        onView(withText("Add Item")).perform(click());
         onView(withId(R.id.item_name_text)).perform(replaceText("Sample Item"));
         onView(withId(R.id.item_make_text)).perform(replaceText("Sample Make"));
         onView(withId(R.id.item_model_text)).perform(replaceText("Sample Model"));
         onView(withId(R.id.item_acquisition_date_text)).perform(replaceText("01/01/2023"));
         onView(withId(R.id.item_value_text)).perform(replaceText("1000"));
         onView(withId(R.id.item_comments_text)).perform(replaceText("Sample Comment"));
-        onView(withText("OK")).perform(click());
+        onView(withId(R.id.add_tag)).perform(click());
+        onView(withText("Test Tag")).perform(click());
+        onView(withId(android.R.id.button1)).perform(click());
+        onView(withId(android.R.id.button1)).perform(click());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     // goes into item details and checks that the details in item details match the item
     // that was clicked
     public void testItemDetailsActivityInfo() {
-        onView(withText("Sample Item")).perform(click());
 
+        onView(withText("Sample Item")).perform(click());
         onView(withId(R.id.itemDescription)).check(matches(withText("Sample Item")));
         onView(withId(R.id.makeText)).check(matches(withText("Sample Make")));
         onView(withId(R.id.modelText)).check(matches(withText("Sample Model")));
         onView(withId(R.id.dateText)).check(matches(withText("01/01/2023")));
         onView(withId(R.id.commentText)).check(matches(withText("Sample Comment")));
-        onView(withId(R.id.valueText)).check(matches(withText("10.00")));
+        onView(withId(R.id.valueText)).check(matches(withText("1000.00")));
+    }
+
+
+    @Test
+    // goes into item details and checks that the details in item details match the item
+    // that was clicked
+    public void testEditItemTags() {
+
+        onView(withId(R.id.add_element)).perform(click());
+        // Click on the "Add Tag" option
+        onView(withText("Add Tag")).perform(click());
+        // Type tag name in the EditText
+        onView(withId(R.id.tag_name_text)).perform(typeText("Test Tag1"), closeSoftKeyboard());
+        onView(withId(android.R.id.button1)).perform(click());
+
+        onView(withId(R.id.add_element)).perform(click());
+        // Click on the "Add Tag" option
+        onView(withText("Add Tag")).perform(click());
+        // Type tag name in the EditText
+        onView(withId(R.id.tag_name_text)).perform(typeText("Test Tag2"), closeSoftKeyboard());
+        onView(withId(android.R.id.button1)).perform(click());
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withText("Sample Item")).perform(click());
+        onView(withId(R.id.itemDescription)).check(matches(withText("Sample Item")));
+        onView(withId(R.id.makeText)).check(matches(withText("Sample Make")));
+        onView(withId(R.id.modelText)).check(matches(withText("Sample Model")));
+        onView(withId(R.id.dateText)).check(matches(withText("01/01/2023")));
+        onView(withId(R.id.commentText)).check(matches(withText("Sample Comment")));
+        onView(withId(R.id.valueText)).check(matches(withText("1000.00")));
+        onView(withId(R.id.edit_item)).perform(click());
+        onView(withId(R.id.edit_tags)).perform(click());
+        onView(withText("Test Tag1")).perform(click());
+        onView(withText("Test Tag")).perform(click());
+        onView(withId(android.R.id.button1)).perform(click());
+        onView(withId(android.R.id.button1)).perform(click());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withText("Sample Item")).perform(click());
+        onView(withId(R.id.edit_item)).perform(click());
+        onView(withId(R.id.edit_tags)).perform(click());
+        onView(withText("Test Tag")).check(matches(isNotChecked()));
+        onView(withText("Test Tag1")).check(matches(isChecked()));
+        onView(withId(android.R.id.button1)).perform(click());
+        onView(withId(android.R.id.button1)).perform(click());
+
     }
 
     @Test
     //test that the back button works by going into item details activity then clicking back
     // then it checks for the search_list which is only in Main Activity
     public void testBackButton() {
+
         onView(withText("Sample Item")).perform(click());
         Espresso.pressBack();
         onView(withId(R.id.search_list)).check(matches(isDisplayed()));
@@ -66,6 +141,7 @@ public class ItemDetailsActivityTest {
     // the delete works
     @Test
     public void testDeleteItem() {
+
         onView(withText("Sample Item")).perform(click());
         onView(withId(R.id.delete_item)).perform(click());
 
