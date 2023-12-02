455dd285b6ba1306f8f466fe3f3e8d427b5a8175
t-rugg <70915141+t-rugg@users.noreply.github.com>
2023-12-01 19:28:43 -0700

    Google authentication sign in and sign out
    Currently does not differentiate items by users, but the actual authentication process works.

---
+++ app/app/src/main/java/com/example/breadheadsinventorymanager/UserLoggedInActivity.java
@@ -0,0 +1,74 @@
+package com.example.breadheadsinventorymanager;
+
+import static java.security.AccessController.getContext;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
+/**
+ * Activity to view or log out a currently-logged-in user.
+ * Some code adapted from <a href="https://developers.google.com/identity/sign-in/android/sign-in">...</a>
+ *
+ * @version 1
+ */
+public class UserLoggedInActivity extends AppCompatActivity implements View.OnClickListener {
+    private GoogleSignInAccount account;
+    private Authenticator auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_logged_in);
+        account = getIntent().getParcelableExtra("account");
+        // enable the back button
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        auth = new Authenticator(FirebaseAuth.getInstance(), this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
+    }
+
+    /**
+     * Handles the top bar to enable the back button.
+     * @param item The menu item that was selected.
+     * @return true
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            Intent intent = new Intent(this, MainActivity.class);
+            this.startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Handles clicking the sign-out button.
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.sign_out_button) {
+            auth.signOut(this);
+            this.startActivity(new Intent(this, UserActivity.class));
+        }
+    }
+}
